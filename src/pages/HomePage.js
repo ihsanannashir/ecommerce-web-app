87ef3c6117ee71cbdc064e504cdfe2d3e4568a9a
ihsanannashir <ihsanannashir@gmail.com>
2022-04-12 23:45:21 +0700

    Added section BrowseRoom

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
+import BrowseRoom from '../parts/HomePage/BrowseRoom'
 import JustArrived from '../parts/HomePage/JustArrived'
 
 export default function HomePage(props) {
@@ -8,6 +9,7 @@ export default function HomePage(props) {
     <>
     <Header/>
     <Hero/>
+    <BrowseRoom/>
     <JustArrived/>
     </>
   )
