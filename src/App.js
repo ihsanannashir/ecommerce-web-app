b3c704a0f468e90cef7d701eee15c7eee3c48447
ihsanannashir <ihsanannashir@gmail.com>
2022-04-15 14:27:56 +0700

    Added Cart section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './assets/css/app.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Details from './pages/Details';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Route exact path="/" component={HomePage}/>
         <Route path="/categories/:idc" component={Details}/>
+        <Route path="/cart" component={Cart}/>
       </Router>
       
     </div>
