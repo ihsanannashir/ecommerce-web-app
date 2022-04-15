20baaa9bbf8ddd2a0d2d6a883f6e133a4d1447d0
ihsanannashir <ihsanannashir@gmail.com>
2022-04-15 14:56:27 +0700

    Added Congratulations section and error page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from '../parts/Header'
+import Footer from '../parts/Footer'
+import Sitemap from '../parts/Sitemap'
+import PageErrorMessage from '../parts/PageErrorMessage'
+
+export default function NotFound() {
+  return (
+    <>
+    <Header theme="black"/>
+    <PageErrorMessage/>
+    <Sitemap/>
+    <Footer/>
+    </>
+  )
+}
